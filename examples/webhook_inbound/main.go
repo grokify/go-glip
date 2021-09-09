c1819b9fd6c6681af43ead3cafa7aed37c6bc98b
John Wang <johncwang@gmail.com>
2021-09-09 02:48:28 -0700

    feat: add card example

--- examples/webhook_inbound/main.go
+++ examples/webhook_inbound/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"strings"
 
 	"github.com/grokify/simplego/fmt/fmtutil"
+	"github.com/grokify/simplego/io/ioutilmore"
+	"github.com/grokify/simplego/net/http/httpsimple"
 	"github.com/jessevdk/go-flags"
 	"github.com/valyala/fasthttp"
 
@@ -14,12 +17,17 @@ import (
 )
 
 const (
-	DEFAULT_URL = "https://hooks.glip.com/webhook/1111-deadbeef-8888"
+	DEFAULT_URL           = "https://hooks.glip.com/webhook/1111-deadbeef-8888"
+	ExampleTypeCard       = "card"
+	ExampleTypeSimple     = "simple"
+	ExampleTypeAlert      = "alert"
+	ExampleTypeAttachment = "attachment"
+	ExampleTypeSalesforce = "salesforce"
 )
 
 type cliOptions struct {
 	WebhookUrlOrGuid string `short:"u" long:"url" description:"URL or GUID for Webhook" required:"true"`
-	Type             string `short:"t" long:"type" description:"Type [simple,card,salesforce,alert]" required:"true"`
+	Type             string `short:"t" long:"type" description:"Type [simple,attachment,salesforce,alert]" required:"true"`
 }
 
 func main() {
@@ -34,31 +42,44 @@ func main() {
 		log.Fatal(err)
 	}
 
-	msgs := []glipwebhook.GlipWebhookMessage{}
-
-	opts.Type = strings.ToLower(strings.TrimSpace(opts.Type))
-	switch opts.Type {
-	case "simple":
-		msgs = append(msgs, examples.ExampleHookBodySimple())
-	case "salesforce":
-		msgs = append(msgs, examples.ExampleHookBodySalesforce())
-	case "alert":
-		msgs = append(msgs, examples.ExampleHookBodyAlert())
-	case "card":
-		msgs = append(msgs, examples.ExampleHookBodyAttachment())
-	default:
-		log.Fatal(fmt.Sprintf("body type not found [%s]", opts.Type))
-	}
-
-	fmtutil.PrintJSON(msgs)
-	fmt.Printf("NUM_MSGS [%v]\n", len(msgs))
-
-	for _, msg := range msgs {
-		req, resp, err := client.PostMessageFast(msg)
-		if err == nil {
-			fmt.Println(string(resp.Body()))
+	if opts.Type == ExampleTypeCard {
+		resp, err := httpsimple.Do(httpsimple.SimpleRequest{
+			Method: http.MethodPost,
+			URL:    opts.WebhookUrlOrGuid,
+			Body:   examples.ExampleHookBodyCardBytes(),
+			IsJSON: true})
+		if err != nil {
+			log.Fatal(err)
 		}
-		fasthttp.ReleaseRequest(req)
-		fasthttp.ReleaseResponse(resp)
+		fmt.Printf("STATUS [%d]\n", resp.StatusCode)
+		fmt.Println(string(ioutilmore.ReadAllOrError(resp.Body)))
+	} else {
+		msgs := []glipwebhook.GlipWebhookMessage{}
+
+		opts.Type = strings.ToLower(strings.TrimSpace(opts.Type))
+		switch opts.Type {
+		case ExampleTypeSimple:
+			msgs = append(msgs, examples.ExampleHookBodySimple())
+		case ExampleTypeSalesforce:
+			msgs = append(msgs, examples.ExampleHookBodySalesforce())
+		case ExampleTypeAlert:
+			msgs = append(msgs, examples.ExampleHookBodyAlert())
+		case ExampleTypeAttachment:
+			msgs = append(msgs, examples.ExampleHookBodyAttachment())
+		default:
+			log.Fatal(fmt.Sprintf("body type not found [%s]", opts.Type))
+		}
+
+		fmtutil.PrintJSON(msgs)
+		fmt.Printf("NUM_MSGS [%v]\n", len(msgs))
+
+		for _, msg := range msgs {
+			req, resp, err := client.PostMessageFast(msg)
+			if err == nil {
+				fmt.Println(string(resp.Body()))
+			}
+			fasthttp.ReleaseRequest(req)
+			fasthttp.ReleaseResponse(resp)
+		}
 	}
 }
--- go.mod
+++ go.mod
@@ -1,14 +1,13 @@
 module github.com/grokify/go-glip
 
-go 1.15
+go 1.16
 
 require (
 	github.com/caarlos0/env v3.5.0+incompatible
 	github.com/grokify/go-ringcentral-client v0.3.0
 	github.com/grokify/oauth2more v0.6.4
-	github.com/grokify/simplego v0.30.8
+	github.com/grokify/simplego v0.30.9
 	github.com/jessevdk/go-flags v1.5.0
-	github.com/joho/godotenv v1.3.0
 	github.com/rs/zerolog v1.24.0
 	github.com/valyala/fasthttp v1.29.0
 )
