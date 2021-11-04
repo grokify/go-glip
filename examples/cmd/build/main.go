798f3ad19a6f3c78c6444d463bb031a6201f4cfa
John Wang <johncwang@gmail.com>
2021-11-04 11:04:50 -0700

    feat: examples: add json files

---
+++ examples/cmd/build/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/grokify/go-glip/examples"
+)
+
+func main() {
+	exps := examples.ExampleWebhooks()
+	fmt.Printf("%v\n", exps)
+
+	for _, exp := range exps {
+		min, err := json.Marshal(exp.Message)
+		if err != nil {
+			log.Fatal(err)
+		}
+		max, err := json.MarshalIndent(exp.Message, "", "  ")
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = os.WriteFile(
+			fmt.Sprintf("example_hook_%s_min.json", exp.Stub), min, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = os.WriteFile(
+			fmt.Sprintf("example_hook_%s_sp2.json", exp.Stub), max, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	fmt.Println("DONE")
+}
