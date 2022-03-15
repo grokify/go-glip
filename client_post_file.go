cfc8c414123c59ce0bf93b02a061b0560189f878
John Wang <johncwang@gmail.com>
2022-03-15 02:08:32 -0700

    dep: go mod: update

--- client_post_file.go
+++ client_post_file.go
@@ -2,18 +2,17 @@ package glip
 
 import (
 	"fmt"
-	"mime"
 	"net/http"
 	"net/url"
 	"os"
 	"path/filepath"
-	"regexp"
 
+	"github.com/grokify/mogo/mime/mimeutil"
 	"github.com/grokify/mogo/net/httputilmore"
 	"github.com/grokify/mogo/net/urlutil"
 )
 
-func PostFile(client *http.Client, serverURL, groupID string, path string) (*http.Response, error) {
+func PostFile(client *http.Client, serverURL, groupID, path string) (*http.Response, error) {
 	file, err := os.Open(path)
 	if err != nil {
 		return nil, err
@@ -36,12 +35,10 @@ func PostFile(client *http.Client, serverURL, groupID string, path string) (*htt
 		return nil, err
 	}
 
-	rs := regexp.MustCompile(`(.[^.]+)$`).FindStringSubmatch(path)
-	if len(rs) < 2 {
-		return nil, err
-	}
-	req.Header.Add(httputilmore.HeaderContentType, mime.TypeByExtension(rs[1]))
-	req.Header.Add(httputilmore.HeaderContentDisposition, fmt.Sprintf(`attachment; filename="%s"`, filename))
+	req.Header.Add(httputilmore.HeaderContentType,
+		mimeutil.MustTypeByFile(path, true))
+	req.Header.Add(httputilmore.HeaderContentDisposition,
+		fmt.Sprintf(`attachment; filename="%s"`, filename))
 
 	return client.Do(req)
 }
--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/grokify/go-ringcentral-client v0.3.5
 	github.com/grokify/goauth v0.13.1
 	github.com/grokify/gohttp v0.2.0
-	github.com/grokify/mogo v0.37.4
+	github.com/grokify/mogo v0.37.6
 	github.com/jessevdk/go-flags v1.5.0
 	github.com/rs/zerolog v1.26.1
 	github.com/valyala/fasthttp v1.34.0
